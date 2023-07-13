4820f60dec84cf0e0e1f59af8a40e800c17545f4
AndreyNedelevich <nedelevich@gmail.com>
2023-07-13 16:30:44 +0300

    Initial commit

---
+++ store/cart-context.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CartContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  removeItem: (id) => {},
+  clearCart: () => {},
+});
+//Добавляем новую функцию clearCart в CONTEXT
+
+export default CartContext;
